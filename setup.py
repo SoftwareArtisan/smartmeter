050bdca415005659865c3231288c1504b153615f
Mandar U Jog <mjog@plotwatt.com>
2013-07-02 03:44:31 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,3 +9,28 @@ import sys
 VERSION = '0.1'
 
 
+SETUP_DICT = dict(
+    name='smartserver',
+    packages=['egauge'],
+    version=VERSION,
+    author='mandarjog',
+    author_email='mandarjog@gmail.com',
+    url='https://github.com/PlotWatt/Smartmeters',
+    description='smart server config',
+    long_description="smart server config",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+  )
+
+# generate .rst file with documentation
+#open(os.path.join(os.path.dirname(__file__), 'documentation.rst'), 'w').write(DOCUMENTATION)
+
+setup(**SETUP_DICT)
