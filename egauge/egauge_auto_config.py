6ecc52bb7327416529a9f7d7f0cf6c55450d0773
Mandar U Jog <mjog@plotwatt.com>
2013-12-11 19:01:51 +0000

    return a version that uses phase consistent naming

--- egauge/egauge_auto_config.py
+++ egauge/egauge_auto_config.py
@@ -59,7 +59,7 @@ def auto_phase_match(cfg, samples=8):
 MIN_CURRENT = 3.0
 
 
-def phase_match(data):
+def phase_match(data, verbose=False):
     """
     look at data and output the best configuration
     """
@@ -72,44 +72,44 @@ def phase_match(data):
     cfg_rot = [sorted(data[cfgdx][0][1], key=lambda v: v.id) for cfgdx in range(3)]
     from copy import copy
     newRegs = sorted(copy(cfg_rot[0]), key=lambda v: v.id)
-    for idx, nr in enumerate(newRegs):
-        print idx, nr
+    if verbose:
+        for idx, nr in enumerate(newRegs):
+            print idx, nr
     #from IPython.core.debugger import Pdb; Pdb().set_trace()
     for idx in range(len(rot[0])):
         ct = [(dx, rot[dx][idx]) for dx in range(3) if rot[dx][idx].I > MIN_CURRENT]
+        flip = False
 
         if len(ct) == 0:
-            print "currents too low to determine correctness"
-            continue
-
-        by_pf = sorted(ct, key=lambda v: v[1].pf, reverse=True)
-        # intialize to no-change
-        cts = ct[0]
-        flip = False
-        if by_pf[0][1].P < 0.0:
-            if by_pf[0][1].I < 10.0:
-                # low current
-                if by_pf[1][1].P > 0.0 and by_pf[1][1].pf > 0.5:
-                    # low pf at low load for motors
-                    cts = by_pf[1]
+            print "Current too low", rot[0][idx].ct
+            cts = (0, rot[0][idx])
+        else:
+            by_pf = sorted(ct, key=lambda v: v[1].pf, reverse=True)
+            # intialize to no-change
+            cts = ct[0]
+            if by_pf[0][1].P < 0.0:
+                if by_pf[0][1].I < 10.0:
+                    # low current
+                    if by_pf[1][1].P > 0.0 and by_pf[1][1].pf > 0.5:
+                        # low pf at low load for motors
+                        cts = by_pf[1]
+                    else:
+                        # flip CT
+                        flip = True
+                        cts = by_pf[0]
                 else:
                     # flip CT
                     flip = True
                     cts = by_pf[0]
             else:
-                # flip CT
-                flip = True
+                # we good
                 cts = by_pf[0]
-        else:
-            # we good
-            cts = by_pf[0]
 
         reg = cfg_rot[cts[0]][idx]
 
         val = reg.val
         if flip is True:
-            print "Flipping {}".format(cts)
-
+            print "Flipping {}: {}".format(cts[1].ct, cts[1])
             if val[0] == '-':
                 val = val[1:]
             else:
