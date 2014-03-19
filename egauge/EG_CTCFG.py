c27fefabfd3b960f6b2d7c65eb05727328623034
Mandar U Jog <mandarjog@gmail.com>
2014-03-19 14:11:54 -0400

    update to support version 2.0+ with new calibration factors

--- egauge/EG_CTCFG.py
+++ egauge/EG_CTCFG.py
@@ -35,11 +35,51 @@ CT_CFG_TABLE=[["5A",None,241.323,"","5.6@1:3@10:2.5@100"],
 ["150A DC",359,-9.16,"","0@50"],
 ["custom",None,0.000,"",""],
 ]
+# firmware version 2.01 use new calibration factors
+CT_CFG_TABLE2=[["5A",None,241.323,"","5.6@1:3@10:2.5@100"],
+["10A",None,121.873,"","5.6@1:3@10:2.5@100"],
+["15A",None,81.519,"","5.6@1:3@10:2.5@100"],
+["20A",None,61.25,"","5.6@1:3@10:2.5@100"],
+["25A",None,49.039,"","5.6@1:3@10:2.5@100"],
+["30A",None,40.897,"","5.6@1:3@10:2.5@100"],
+["50A",None,24.509,"","5.6@1:3@10:2.5@100"],
+["60A",None,20.485,"","5.6@1:3@10:2.5@100"],
+["70A",None,17.5,"","4.5@0:2.9@3:2.4@30:2.1@100"],
+["75A",None,16.409,"","5.6@1:3@10:2.5@100"],
+["100A",None,12.281,"","4.5@0:2.9@3:2.4@30:2.1@100"],
+["150A",None,8.194,"","4.5@0:2.9@3:2.4@30:2.1@100"],
+["200A",None,6.148,"","5.6@1:3@8:1.5@100"],
+["300A",None,4.101,"","5.6@1:3@8:1.5@100"],
+["400A",None,3.075,"","5.6@1:3@8:1.5@100"],
+["600A",None,2.051,"","5.6@1:3@8:1.5@100"],
+["800A",None,1.538,"","5.6@1:3@8:1.5@100"],
+["1000A",None,1.231,"","5.6@1:3@8:1.5@100"],
+["1200A",None,1.026,"","5.6@1:3@8:1.5@100"],
+["1500A",None,0.821,"","5.6@1:3@8:1.5@100"],
+["2000A",None,0.616,"","5.6@1:3@8:1.5@100"],
+["3000A",None,0.411,"","5.6@1:3@8:1.5@100"],
+["4000A",None,0.308,"","5.6@1:3@8:1.5@100"],
+["AccuCT  50A",None,24.642,"","0.33@3:0.262@6:0.007@50:-0.192@100"],
+["AccuCT 100A",None,12.33,"",".27@1.5:.24@3:.20@6:.08@15:-.09@50:-.27@100"],
+["AccuCT 200A",None,6.165,"",".3@.1:.32@1:.33@1.5:.31@3:.28@6:.19@15:0@50:-.16@100"],
+["250A rope",None,4.925,"",""],
+['Rope 6',None,0.191,"int","6@50"],
+[" 10A DC",359,-137.4,"","0@50"],
+[" 20A DC",359,-68.7,"","0@50"],
+[" 50A DC",359,-27.48,"","0@50"],
+["100A DC",359,-13.74,"","0@50"],
+["150A DC",359,-9.16,"","0@50"],
+["custom",None,0.000,"",""],
+]
 
-CT_CFG = dict( [ (r[0].strip(), r) for r in CT_CFG_TABLE ])
-CT_CFG_BY_MULTIPLIER = dict( [ ("{:.3f}".format(r[2]), r) for r in CT_CFG_TABLE ])
+CT_CFG = dict([(r[0].strip(), r) for r in CT_CFG_TABLE])
+CT_CFG_BY_MULTIPLIER = dict([("{:.3f}".format(r[2]), r) for r in CT_CFG_TABLE])
+
+CT_CFG2 = dict([(r[0].strip(), r) for r in CT_CFG_TABLE2])
+CT_CFG_BY_MULTIPLIER2 = dict([("{:.3f}".format(r[2]), r) for r in CT_CFG_TABLE2])
 
 CT_CFG['Rope CT'] = CT_CFG['Rope 6']
+CT_CFG2['Rope CT'] = CT_CFG2['Rope 6']
 
 # CT and PT configuration is ordered in a peculiar way.
 CT_MAP = {
@@ -61,7 +101,8 @@ CT_MAP = {
         }
 CT_MAP_REV = dict( ( val, key ) for (key, val) in CT_MAP.items() )
 
-def get_ch_row(ct_type, calibration=None, mul=None):
+
+def get_ch_row(ct_type, calibration=None, mul=None, version="2.0"):
     """
     return a POST config row like 
     for a Rope 6" with calibration of 4.6200 
@@ -71,12 +112,15 @@ def get_ch_row(ct_type, calibration=None, mul=None):
     or 400A and other CTS
     ch7=3.0750,15,,,5.6@1:3@8:1.5@100
     """
+    _CT_CFG = CT_CFG
+    if version >= "2.0":
+        _CT_CFG = CT_CFG2
     # for potential transformers the following are the defaults
     if ct_type == 'PT':
         return "-4.0030,-2058,,,"
 
-    if ct_type not in CT_CFG:
-        raise Exception ("Unknown CT type {}".format(ct_type))
+    if ct_type not in _CT_CFG:
+        raise Exception("Unknown CT type {}".format(ct_type))
 
     if calibration is not None:
         calibration = float(calibration)
@@ -84,13 +128,13 @@ def get_ch_row(ct_type, calibration=None, mul=None):
     if mul is not None:
         mul = float(mul)
 
-    cfg = CT_CFG[ct_type]
+    cfg = _CT_CFG[ct_type]
     # all CT_CFG_TABLE have this in the front
     if mul is None:
         mul = cfg[2]
     ch_row = "{:.4f},15,".format(mul)
 
-    if ct_type in [ 'Rope 6', 'Rope CT' ]:
+    if ct_type in ['Rope 6', 'Rope CT']:
         if calibration is None:
             raise Exception('calibration is required for Rope 6" CT')
         ch_row += "{},{:.4f},{}".format(cfg[3], calibration, cfg[-1])
@@ -101,6 +145,19 @@ def get_ch_row(ct_type, calibration=None, mul=None):
         ch_row += ",,{}".format(cfg[-1])
 
     return ch_row
+
+
+def get_ct_type_by_mul(mul_str, version="2.0"):
+    """
+    return a ct type version o
+    """
+    ct_type = None
+    _CT_CFG_BY_MULTIPLIER = CT_CFG_BY_MULTIPLIER
+    if version >= "2.0":
+        _CT_CFG_BY_MULTIPLIER = CT_CFG_BY_MULTIPLIER2
+    if mul_str in _CT_CFG_BY_MULTIPLIER:
+        ct_type = _CT_CFG_BY_MULTIPLIER[mul_str][0]
+    return ct_type
 
 
 import unittest
